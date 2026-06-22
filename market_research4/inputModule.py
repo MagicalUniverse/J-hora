089b08a716961a5a135c036580705855d7b09712
MagicalUniverse <108sanskrit@gmail.com>
2026-06-22 22:22:57 +0200

    Update inputModule.py

--- market_research4/inputModule.py
+++ market_research4/inputModule.py
@@ -1,14 +1,7 @@
 from datetime import datetime, time
-
 class InputModule:
     def __init__(self, lat, lon, dt_birth, dt_transit_start=None):
         self.lat = lat
         self.lon = lon
-        self.dt_birth = dt_birth  # Fixed reference for scanning
-        
-        # Default: 09:00 on the day of entry if not provided
-        if dt_transit_start is None:
-            today = datetime.now().date()
-            self.dt_transit_start = datetime.combine(today, time(9, 0))
-        else:
-            self.dt_transit_start = dt_transit_start
+        self.dt_birth = dt_birth
+        self.dt_transit_start = dt_transit_start or datetime.combine(datetime.now().date(), time(9, 0))
